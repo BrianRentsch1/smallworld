aa387291af5133a470457e44cec5f595280b82e4
Brian Rentsch <brentsch882@gmail.com>
2018-12-02 11:06:47 -0500

    Added command line argument support

--- main.cpp
+++ main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <string>
 #include <stdlib.h>
 #include <time.h>
 #include "Graph.h"
 
 using namespace std;
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    int m;
+    
+    if(argc == 1)
+    {
+        cout << endl << "Usage: \t./project2 <M value>\n" << endl;
+        return 1;
+    }
+    else if(argc > 2)
+    {
+        cout << "Error: too many arguments" << endl;
+        return 1;
+    }
+    else
+    {
+        m = stoi(argv[1]);        
+    }
+    
     Graph *g = new Graph();
 
-    g->populateGraph(g, 100);
+    g->populateGraph(g, m);
     g->printGraph(g);
     
     return 0;
