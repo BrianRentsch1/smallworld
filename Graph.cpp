ac193021ff45500f1677b670ea6753bb17103cef
Brian Rentsch <brentsch882@gmail.com>
2018-12-02 02:38:01 -0500

    Changed things significantly. Fixed an issue in which many nodes were not getting their full degree of edges. Improved duplicate edge detection. Introduced infinite loop safeguards. Added some error handling when the M value was too high.

--- Graph.cpp
+++ Graph.cpp
@@ -12,19 +12,25 @@ Graph::Graph()
 }
 
 //Initialize new graph
-void Graph::initializeGraph(Graph *g, int m)
+int Graph::initializeGraph(Graph *g, int m)
 {
     srand(time(NULL));
-    g->num_nodes = 100000;
+    g->num_nodes = 0;
     g->num_edges = 0;
+    if((4*m)/3 > MAX_NODES)
+    {        
+        return 1;
+    }
     g->m = m;
     
     for(int i = 0; i < MAX_NODES; i++) //For all nodes, set random degree and initialize place in adjecency list as null
     {
-        g->node_edges[i] = 0;   //Currently no edges in graph
-        g->node_degrees[i] = rand() % ((2*g->m)/3) + ((2*g->m)/3);  //generate random degree between 2/3m and and 4/3m for each node
+        g->node_edges[i] = 0;   //Currently no edges in graph, so each node has 0 edges
+        g->node_degree[i] = rand() % (((4*g->m)/3) - ((2*g->m)/3) + 1) + ((2*g->m)/3);  //generate random degree between 2/3m and and 4/3m for each node
         g->edges[i] = new node(i);
+        num_nodes++;
     }
+    return 0;
 }
 
 //Insert an edge into the graph
@@ -36,47 +42,144 @@ void Graph::insertEdge(Graph *g, int x, int y, bool duplicate)
     g->node_edges[x] += 1;  //Node x has one more edge 
     if(duplicate == false)
     {
-        insertEdge(g,y,x,true); //This is undirected graph, so insert an edge in y's list    
+        insertEdge(g,y,x,true);  //This is undirected graph, so insert an edge in y's list    
     }
-    else g->num_edges += 1;    
+    g->num_edges += 1;    
 }
 
 void Graph::populateGraph(Graph *g, int m)
 {
     int y;
-    
-    initializeGraph(g, m);
+    int invalid_graph;
+    invalid_graph = initializeGraph(g, m);
+    if(invalid_graph == 1) //Make sure the M value chosen is realistic
+    {
+        cout << "Invalid graph! Please select an M value such that 4m/3 is less than or equal to " << MAX_NODES << endl;
+        cout << "Currently, m = " << m << " and 4m/3 = " << (4*m)/3 << endl;
+        return;
+    }    
+    cout << "******Populating graph******" << endl;
+    if(m > 150)
+    {
+        cout << "This might take a while..." << endl;
+    }
+    graph_populated = 1;
     srand(time(NULL));
-       
+    
     for(int i = 0; i < MAX_NODES; i++)  //For each node...
     {
-        for(int j = 0; g->node_edges[i] < g->node_degrees[i]; j++) //Keep adding edges until the number of edges for node[i] == the degree of node[i]
+        node *p = g->edges[i];  //temporary node pointer
+
+        //Variables used to prevent looping (used for small MAX_NODE values only
+        bool unique_found;
+        bool all_possibilities_tested = false;
+        //       int past_ys[MAX_NODES];            
+        int uniques_tested = 0;
+
+        int loop_counter = 0;   //Value to prevent looping at large MAX_NODE values
+
+        /*The purpose of this array is to hold all unique random y values we get. We do this in order to see if we have tested every possible y value, 
+          in case it is impossible to completely populate a node due to limitations based on the M value. What I mean by this is, if all people only know a 
+          small amount of people (M people on average), then those relationships get filled quickly while populating the graph. Each node has a respective 
+          degree, meaning they should have DEGREE edges to other nodes, but this may not be possible if the other nodes have already filled their own 
+          degrees, and if the remaining eligible nodes without their degrees filled are already edges connected to the node in question. Thus, incomplete 
+          nodes are left in the graph. "Incomplete" meaning they have a degree higher than the number of unique edges that could be given to them.*/
+        /* if(MAX_NODES < 1000)   //This operation becomes too expensive at large MAX_NODE values
+        {        
+            for(int p = 0; p < MAX_NODES; p++)
+            {
+                past_ys[p] = -1;   //Fill array of previously tested ys with -1s ("indicating blanks")
+            }
+            }*/
+        for(int j = 0; g->node_edges[i] < g->node_degree[i]; j++) //Keep adding edges until the number of edges for node[i] == the degree of node[i]
         {
-            int inserted_nodes[g->node_degrees[i]];  //Make an array of all nodes added to current index of adjecency list
-            bool in_list;
+            bool edge_exists; //Indicate if we have found a duplicate edge
             do
             {
-                y = rand() % MAX_NODES;  //Generate a random node to make an edge with
-                for(int k = 0; k < g->node_degrees[i];k++)
+                p = g->edges[i]; //Point p to the top of the current node's linked list
+
+                y = rand() % MAX_NODES;  //Generate a random node to make an edge with                
+
+                /*   if(MAX_NODES < 1000)    //Complete this processing only if we are using the "past_ys" array previously declared
                 {
-                    if(inserted_nodes[k] == y)
-                        in_list = true;
+                    unique_found = true;  //Start with unique_found being true
+                    
+                    for(int p = 0; p < MAX_NODES; p++)   //Search the array of past y's
+                    {
+                        if (past_ys[p] == y)   //If we find a duplicate
+                        {
+                            unique_found = false;   //...we won't add it to the array
+                        }
+                    }
+                    if(unique_found == true)    //If we find a unique value
+                    {
+                        past_ys[uniques_tested] = y;   //...add it to the array
+                        uniques_tested++;   //We found one more unique y value
+                    }
+                    if(uniques_tested >= MAX_NODES)   //If we have found all possible unique y values (and none of them were adequate to make a unique edge)
+                    {
+                        all_possibilities_tested = true;   //...we need to leave this loop, else we will loop forever
+                        break;
+                        }
+                }*/
+
+                //Traverse linked list of our current node to find if edge (x,y) already exists                
+                while(p != nullptr)
+                {
+                    if(node_edges[y] == 0) //If there are no edges connected to this node...
+                    {                        
+                        edge_exists = false; //Currently no edges connected to this node -- OK to insert any edge (besides (x, x))  
+                    }
+                    else if(p->id == y)   //If a node (x, y) already exists... 
+                    {
+                        edge_exists = true;  //Edge will be a duplicate -- don't insert it and stop traversing this node's linked list
+                        break;
+                    }
+                    else  //Otherwise, the edge (x, y) doesn't exist, and can be inserted
+                    {
+                        edge_exists = false;  //Edge will not be a duplicate -- OK to insert
+                    }
+                    
+                    p = p->next;
                 }
-            } while(y == i && !in_list);
-            insertEdge(g,i,y,false);
-        }        
+                
+                if(loop_counter > 100000)  //For large MAX_NODE values, this is a safeguard to prevent infinite looping (since we don't keep track of all y values tested to see if this node will be incomplete)
+                {
+                    break;
+                }
+                
+                loop_counter++;
+
+            } while(y == i || node_edges[y] == node_degree[y] || edge_exists);  //Repeatedly pick a new y value if node y already has its full degree of edges or if (x,y) is an edge that already exits
+
+            if(all_possibilities_tested == false)  //If this node did not exhaust all possible y values, we insert an edge
+            {
+                insertEdge(g,i,y,false);
+            }
+            else  //Otherwise, we break and proceed to the next node, as no other edges can be added to this node
+            {
+                break;
+            }
+        }
     }    
 }
 
 void Graph::printGraph(Graph *g)
 {
-    node *p;
+    if(graph_populated == 0)  //Handle being called when graph has yet to be populated
+    {
+        cout << "Error: please populate a graph with a valid M value first" << endl;
+        return;
+    }
+    
+    node *p;   //temporary pointer
 
     for(int i = 0; i < g->num_nodes; i++)
     {
-        int first = 1;
+        int first = 1;  //Keep track of whether or not this is the head of the current linked list, for special formatting
+        
         printf("%d: ", i);
-        p = g->edges[i];        
+        p = g->edges[i];
         
         while (p != NULL)
         {
@@ -84,10 +187,18 @@ void Graph::printGraph(Graph *g)
             {
                 p = p->next;
                 first = !first;
-            }         
-            printf(" %d", p->id);                        
-            p = p->next;        
+            }
+            printf(" %d", p->id);
+            p = p->next;
         }
-        printf("\n");
+        if(g->node_degree[i] != g->node_edges[i])
+        {
+            printf(" // expected: %d actual: %d\n", g->node_degree[i], g->node_edges[i]);
+            g->incomplete_nodes += 1;
+        }
+        else cout << endl;
     }
+    cout << "Total nodes: " << g->num_nodes << endl;
+    cout << "Total edges: " << g->num_edges << endl;
+    cout << "Total nodes left incomplete: " << g->incomplete_nodes << endl;
 }
--- Graph.h
+++ Graph.h
@@ -6,15 +6,19 @@ class Graph
 {
 public:
     node *edges[MAX_NODES];        //All nodes of our graph
-    int node_degrees[MAX_NODES];   //Degree of each node in our graph
+    int node_degree[MAX_NODES];   //Degree of each node in our graph
     int m;                         //Average degree of graph
     int node_edges[MAX_NODES];     //How many edges are connected to each node
     int num_nodes;                 //Total nodes in graph
     int num_edges;                 //Total edges in graph
+    int incomplete_nodes;          //If any, how many nodes were left incomplete?
+    int graph_populated;           //Indicates when the graph has been successfully populated
     
     Graph();
-    void initializeGraph(Graph *g, int m);   //Initialize graph
-    void populateGraph(Graph *g, int m);
-    void insertEdge(Graph *g, int x, int y, bool duplicate); //Insert edge between nodes x and y
+    void populateGraph(Graph *g, int m);    //Insert random edges into graph   
     void printGraph(Graph *g);               //Print contents of graph
+    
+private:
+    int initializeGraph(Graph *g, int m);   //Initialize graph
+    void insertEdge(Graph *g, int x, int y, bool duplicate); //Insert edge between nodes x and y
 };
